856f70c2a2e0851cb2cfe2bde49ac9ac7731e7b4
Karthickraja <karthickraja.gm@gmail.com>
2024-03-06 20:48:29 +0530

    frontend basic template in html, css and js

---
+++ calc.js
@@ -0,0 +1,32 @@
+$(document).on('click','.location_btn',function(){
+
+    var currentSelectedId = $(this).data('id');
+
+    var pickupLocation = locations.find((item) => item.id === currentSelectedId);
+
+    var RemainingLocation = locations.filter((item) => item.id !== currentSelectedId);
+
+    let html = '<option value="">Drop Location</option>';
+    RemainingLocation.forEach((item,index) => {
+        html += `
+        <option value="${item.id}">${item.name}</option>
+        `;
+    });
+
+    $('#to_location').html(html);
+    $('#from_time').val(0);
+
+    $('#popup_modal').show();
+});
+
+
+$('#modal_close,#btn-close').click(function(){
+    $('#popup_modal').hide();
+});
+
+
+$('#modal_submit').click(function(){
+    
+});
+
+
---
+++ function.js
@@ -0,0 +1,62 @@
+const locations = [
+    {
+        id: 1,
+        name: 'Point A',
+    },
+    {
+        id: 2,
+        name: 'Point B',
+    },
+    {
+        id: 3,
+        name: 'Point C',
+    },
+    {
+        id: 4,
+        name: 'Point D',
+    },
+    {
+        id: 5,
+        name: 'Point E',
+    },
+    {
+        id: 6,
+        name: 'Point F',
+    },
+];
+
+
+const taxis = [
+
+];
+
+
+function addTaxis(length){
+    for (let index = 1; index <= length; index++) {
+        taxis.push(`T${index}`)
+    }
+}
+
+// Add Number Taxis
+addTaxis(4);
+
+function addLocations(){
+    let html = '';
+    locations.forEach((item,index)=>{
+        html += `
+                <div class="m-5">
+                    <button class="button btn-primary p-2 location_btn" data-id="${item.id}">${item.name}</button>
+                </div>
+                `;
+    });
+
+    $('#location_container').html(html);
+}
+
+// add location
+addLocations();
+
+
+
+
+
