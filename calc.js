cc1923d55e918aa5c08bf10196aeb4212b86e2c4
Karthickraja <karthickraja.gm@gmail.com>
2024-03-09 12:23:48 +0530

    functionality

--- calc.js
+++ calc.js
@@ -2,9 +2,9 @@ $(document).on('click','.location_btn',function(){
 
     var currentSelectedId = $(this).data('id');
 
-    var pickupLocation = locations.find((item) => item.id === currentSelectedId);
+    var pickupLocation = taxiLocations.find((item) => item.id === currentSelectedId);
 
-    var RemainingLocation = locations.filter((item) => item.id !== currentSelectedId);
+    var RemainingLocation = taxiLocations.filter((item) => item.id !== currentSelectedId);
 
     let html = '<option value="0">Drop Location</option>';
     RemainingLocation.forEach((item,index) => {
@@ -15,10 +15,28 @@ $(document).on('click','.location_btn',function(){
 
     $('#to_location').html(html);
     $('#from_time').val(0);
-
+    $('#pickup_point').val(currentSelectedId);
     $('#popup_modal').show();
 });
 
+document.getElementById("from_time").addEventListener("input", function() {
+    // Get the input element
+    var inputElement = document.getElementById("from_time");
+    // Get the value of the input
+    var value = inputElement.value;
+    
+    // Check if the value is a non-negative integer
+    if (value !== "" && (isNaN(value) || parseInt(value) != value || value < 0 || value > 24)) {
+        if (value < 0) {
+            inputElement.value = 0;
+        } else if (value > 24) {
+            inputElement.value = 24;
+        } else {
+            // If not an integer, set the value to the closest integer
+            inputElement.value = Math.round(parseFloat(value));
+        }
+    }
+});
 
 $('#modal_close,#btn-close').click(function(){
     $('#popup_modal').hide();
@@ -33,14 +51,17 @@ $('#modal_submit').click(function(){
         alert('Please select drop location');
         return '';
     }
-   
-    bookTaxi(dropLocation,from_time);
+    var pickupLocation = $('#pickup_point').val();
+    bookTaxi(pickupLocation,dropLocation,from_time);
     
 });
 
 
 $('#reset_app').click(function(){
-
+    var res = confirm('Are you sure?');
+    if(res){
+        setDay();
+    }
 });
 
 $('#taxi_details').click(function(){
--- function.js
+++ function.js
@@ -1,30 +1,52 @@
-const locations = [
+const taxiLocations = [
     {
         id: 1,
         name: 'Point A',
+        taxi_stand: [
+
+        ],
     },
     {
         id: 2,
         name: 'Point B',
+        taxi_stand: [
+
+        ],
     },
     {
         id: 3,
         name: 'Point C',
+        taxi_stand: [
+
+        ],
     },
     {
         id: 4,
         name: 'Point D',
+        taxi_stand: [
+
+        ],
     },
     {
         id: 5,
         name: 'Point E',
+        taxi_stand: [
+
+        ],
     },
     {
         id: 6,
         name: 'Point F',
+        taxi_stand: [
+
+        ],
     },
 ];
 
+
+const taxisData = {
+
+};
 
 const taxis = [
 
@@ -33,16 +55,39 @@ const taxis = [
 
 function addTaxis(length){
     for (let index = 1; index <= length; index++) {
-        taxis.push(`T${index}`)
+        taxis.push(`T${index}`);
     }
+
+    taxiLocations.forEach((item,index) => {
+        if(item.id === 1){
+            item.taxi_stand = taxis;
+        }else{
+            
+            item.taxi_stand = [];
+        }
+    });
 }
+
+
+function setDay(){
+    
+    taxiLocations.forEach((item,index) => {
+        if(item.id === 1){
+            item.taxi_stand = taxis;
+        }else{
+            
+            item.taxi_stand = [];
+        }
+    });
+}
+
 
 // Add Number Taxis
 addTaxis(4);
 
 function addLocations(){
     let html = '';
-    locations.forEach((item,index)=>{
+    taxiLocations.forEach((item,index)=>{
         html += `
                 <div class="m-5">
                     <button class="button btn-primary p-2 location_btn" data-id="${item.id}">${item.name}</button>
@@ -57,10 +102,121 @@ function addLocations(){
 addLocations();
 
 
-function bookTaxi(dropLocation,from_time){
-    console.log(dropLocation,from_time);
+function bookTaxi(pickupLocation,dropLocation,from_time){
+    console.log(pickupLocation,dropLocation,from_time);
+    var taxi = findTaxi(pickupLocation);
+
+    console.log(taxi);
+
+    updateTaxiStand(taxi,dropLocation);
+
 }
 
+function updateTaxiStand(taxi,dropLocation){
+    taxiLocations.forEach(item => {
+        var taxi_stand = item.taxi_stand;
+        if(taxi_stand.length > 0){
+            let index;
+            while ((index = taxi_stand.indexOf(taxi)) !== -1) {
+                taxi_stand.splice(index, 1);
+            }
+            item.taxi_stand = taxi_stand;
+        }
+    });
+    let index = dropLocation - 1;
+    if(taxiLocations[index]){
+        let taxi_stand = taxiLocations[index].taxi_stand;
+        taxi_stand.push(taxi);
+        taxi_stand.sort();
+        taxiLocations[index].taxi_stand = taxi_stand;
+    }
+    console.log(taxiLocations);
+}
+
+
+function checkTaxiTime(taxi){
+    return taxi;
+}
+
+function findTaxi(pickupLocation){
+    var totalLength = taxiLocations.length;
+
+    // first check in current location
+    var freeTaxi = [];
+
+    let availableTaxi = taxiLocations[pickupLocation - 1].taxi_stand;
+    if(availableTaxi.length){
+        availableTaxi.forEach((taxi) => {
+            let res = checkTaxiTime(taxi);
+            if(res){
+                freeTaxi.push(taxi);
+            }
+        });
+    }
+    
+    if(freeTaxi.length > 0){
+        return checkEarning(freeTaxi);
+    }
+
+    // if there is not taxi at current location, check next nearest point
+    var leftSide = pickupLocation - 2;
+    var rightSide = pickupLocation;
+    while(true){
+        freeTaxi = [];
+        let pass1 = false;
+        let pass2 = false;
+        if(leftSide >= 0){
+            let availableTaxi = taxiLocations[leftSide].taxi_stand;
+
+            availableTaxi.forEach((taxi) => {
+                let res = checkTaxiTime(taxi);
+                if(res){
+                    freeTaxi.push(taxi);
+                }
+            });
+            pass1 = true;
+
+        }
+        leftSide--;
+
+        if(rightSide <= totalLength -1){
+            let availableTaxi = taxiLocations[rightSide].taxi_stand;
+
+            availableTaxi.forEach((taxi) => {
+                let res = checkTaxiTime(taxi);
+                if(res){
+                    freeTaxi.push(taxi);
+                }
+            });
+            pass2 = true;
+
+        }
+        rightSide++;
+
+        if(freeTaxi.length > 0){
+            return checkEarning(freeTaxi);
+        }
+
+        if(pass1 === false && pass2 === false){
+            break;
+        }
+    }
+
+    return ''
+
+}
+
+function checkEarning(availableTaxi){
+    // need to calc earnings, if more than one
+
+    var taxiName = availableTaxi[0];
+
+    return taxiName;
+
+
+
+
+}
 
 
 
